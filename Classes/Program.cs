576064252b8e432ac528369b06ce8b67b9a310ef
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2022-12-08 12:33:10 -0800

    Done

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Pruis";
+            myCar.year = "2010";
+
+            var Ford = new car()
+            {
+                Make = "Ford",
+                Model = "Raptar",
+                year = "2013"
+            };
+            var BMW = new car("BMW", "M4", 2022);
+
+            var carList = new List<car> { myCar, BMW, Ford };
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Model} {vehicle.Make} {vehicle.year}" );
+            }
+            
         }
+
+        
     }
 }
---
+++ Classes/car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class car
+    {
+        public car(string make, string model, int year)
+        {
+            Make= make;
+            Model= model;
+            year = year;
+        }
+        public car()
+        {
+
+        }
+
+        public string Make { get; set; }   
+        public string Model { get; set; }   
+        public string year { get; set; }
+    }
+}
